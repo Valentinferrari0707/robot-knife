13fa8a874d15a9246dd716090eca37bd8f5f3474
Florent Cadot <florent.cadot@mastertp>
2019-10-10 15:40:09 +0200

    moving robot is working

--- catkin_ws/src/control/scripts/control.py
+++ catkin_ws/src/control/scripts/control.py
@@ -6,6 +6,9 @@ from std_msgs.msg import String
 from std_msgs.msg import Float64
 from scan.srv import LidarDatas
 
+# def demo():
+
+
 def getLidarDatas():
     # Get datas when no hand
     # rospy.wait_for_service('lidar_datas')
@@ -32,16 +35,43 @@ def getLidarDatas():
         print "Service call failed: %s"%e
 
 
-def publisher(pub):
-    # rospy.loginfo(  .get_caller_id() + 'I heard %s', data.data)
-    rospy.sleep(2.)    
-    pub.publish(1.5)
+def move_motors(angleM1,angleM2,angleM3):
+    motor1Pub = rospy.Publisher('/motor1_controller/command', Float64, queue_size=10)
+    motor2Pub = rospy.Publisher('/motor2_controller/command', Float64, queue_size=10)
+    motor3Pub = rospy.Publisher('/motor3_controller/command', Float64, queue_size=10)
+    rospy.sleep(3.)
+    print('motor1 publish')
+    motor1Pub.publish(angleM1)
+    rospy.sleep(1.)
+    print('motor2 publish')
+    motor2Pub.publish(angleM2)   
+    rospy.sleep(1.)
+    print('motor3 publish')
+    motor3Pub.publish(angleM3)
+
+def processLidarDatas():
+    arrayMotorsAngle = [] #[[angleMotor1,angleMotor2,angleMotor3], ...] order => order in the motion sequence 
+    return arrayMotorsAngles
+
+def motorsMotion(seqMotorsAngles):
+    print(seqMotorsAngles)
+    if(len(seqMotorsAngles)!=0):
+        for i in range (0,len(seqMotorsAngles)):
+            move_motors(seqMotorsAngles[i][0],seqMotorsAngles[i][1],seqMotorsAngles[i][2])
+    else:
+        print('motion sequence vide !')
 
 def control():
 
     rospy.init_node('control', anonymous=True)
-    getLidarDatas()
+    # datasLidar = getLidarDatas()
+    # seqMotorsAngles = processLidarDatas(datasLidar) #
+    # motorsMotion(seqMotorsAngles)
+    motorsMotion([[0.5,0.4,-1.2]])
     rospy.sleep(3.)
+
+
+
     # control = rospy.Publisher('/joint1_controller/command', Float64, queue_size=10)
     # rate = rospy.Rate(10) # 10hz
 
